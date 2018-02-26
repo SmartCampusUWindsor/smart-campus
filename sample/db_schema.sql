fd0c1b96a5789384925dec4f8713491c3f3f2b08
Zach <shaverz@uwindsor.ca>
2018-02-26 16:59:17 -0500

    Move db creds out of source code
    For security, move db creds to private_html, just outside of root.
    This also allows devs to use their own db to test messageboard.
    
    Setup: Create db with schema in /sample and create db_config.ini in
    /private_html with your credentials.
    
    * We should change the current database as creds are out in the wild now.

---
+++ sample/db_schema.sql
@@ -0,0 +1,37 @@
+-- This file can be used to initialize a MySQL database for the messageboard
+
+CREATE DATABASE message_board;
+USE message_board;
+
+CREATE TABLE `entries` (
+ `logid` int(11) NOT NULL AUTO_INCREMENT,
+ `title` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+ `log` varchar(5000) COLLATE utf8_unicode_ci NOT NULL,
+ `userid` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+ `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ PRIMARY KEY (`logid`)
+);
+
+CREATE TABLE `users` (
+ `user_name` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+ `create_date` datetime DEFAULT NULL,
+ `password` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+ `first_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+ `last_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+ `street` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+ `city` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+ `prov` char(2) COLLATE utf8_unicode_ci DEFAULT NULL,
+ `postalcode` char(7) COLLATE utf8_unicode_ci DEFAULT NULL,
+ `phone` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+ `email` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+ `isAdmin` int(11) NOT NULL,
+ PRIMARY KEY (`user_name`)
+);
+
+CREATE TABLE `messages` (
+ `msgid` int(11) NOT NULL AUTO_INCREMENT,
+ `msg` varchar(140) COLLATE utf8_unicode_ci NOT NULL,
+ `username` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+ `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ PRIMARY KEY (`msgid`)
+);
